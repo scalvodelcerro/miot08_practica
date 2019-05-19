1f4337dbe6a70233ee53166435004bf112fe8650
scalvo <scalvodelcerro@gmail.com>
2019-05-19 23:32:16 +0200

    Processing

--- processing/build.sbt
+++ processing/build.sbt
@@ -1,6 +1,8 @@
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.7"
 
 name := "Processing"
 organization := "viu.master.iot"
 version := "1.0"
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+libraryDependencies += "com.databricks" %% "spark-xml" % "0.5.0"
--- processing/src/main/scala/Processing.scala
+++ processing/src/main/scala/Processing.scala
@@ -1,8 +1,11 @@
 import org.apache.spark._
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.types.{StructType, StructField, StringType, DoubleType};
+import org.apache.spark.sql.functions._
 import com.databricks.spark.xml._
-/* spark-shell --conf spark.ui.port=4050 --packages com.databricks:spark-xml_2.10:0.4.1 */
+import scala.concurrent.duration._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
 object Processing {
   def main(args: Array[String]) {
@@ -19,18 +22,53 @@ object Processing {
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
     // Read from xml
+    val readTimeStart = System.nanoTime
     val df = sqlContext.read
       .format("com.databricks.spark.xml")
       .option("rowTag","row")
       .load(args(0))
+    val readTimeEnd = System.nanoTime
 
-    // Write to csv
-    df.write
-      .format("com.databricks.spark.csv")
-      .option("header", "true")
-      .option("delimiter", ",")
-      .option("inferSchema", "true")
-      .mode("overwrite")
-      .save("/scalvo/stackoverflow/csv")
+    // Aggregate data 
+    val aggregateTimeStart = System.nanoTime
+    val aggs = df
+      .groupBy("_DisplayName")
+      .agg(Map("_Reputation" -> "max", "_Views" -> "max"))
+      .cache
+    aggs.show
+    val aggregateTimeEnd = System.nanoTime
+
+    // Get top 10 users with most reputation
+    val reputationTimeStart = System.nanoTime
+    val topTenReputation = aggs
+      .sort(desc("max(_Reputation)"))
+      .select("_DisplayName", "max(_Reputation)")
+      .take(10)
+    println("---Top ten users by reputation")
+    println(topTenReputation)
+    val reputationTimeEnd = System.nanoTime
+
+    // Get top 10 users with most views
+    val viewsTimeStart = System.nanoTime
+    val topTenViews = aggs
+      .sort(desc("max(_Views)"))
+      .select("_DisplayName", "max(_Views)")
+      .take(10)
+    println("---Top ten users by views")
+    println(topTenViews)
+    val viewsTimeEnd = System.nanoTime
+
+    // Print stats
+    println("------")
+    println("--- Processing stats")
+    println("")
+    println(" XML read time     : " +
+      Duration( readTimeEnd - readTimeStart , NANOSECONDS).toSeconds + " seconds")
+    println(" Aggregation time  : " +
+      Duration( aggregateTimeEnd - aggregateTimeStart , NANOSECONDS).toSeconds + " seconds")
+    println(" By reputation time: " +
+      Duration( reputationTimeEnd - reputationTimeStart , NANOSECONDS).toSeconds + " seconds")
+    println(" By views time     : " +
+      Duration( viewsTimeEnd - viewsTimeStart , NANOSECONDS).toSeconds + " seconds")
   }
 }
